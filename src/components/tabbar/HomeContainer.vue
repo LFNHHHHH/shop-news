afeae971521c4ff9a4f39eb514e83588fef14927
unknown <>
2020-03-26 20:19:33 +0800

    首页九宫格

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -6,6 +6,46 @@
         <img :src="item.img" />
       </mt-swipe-item>
     </mt-swipe>
+
+    <!-- 九宫格 到 6宫格 的改造工程 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu2.png" />
+          <div class="mui-media-body">新闻资讯</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu2.png" />
+          <div class="mui-media-body">图片分享</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu3.png" />
+          <div class="mui-media-body">商品购买</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu4.png" />
+          <div class="mui-media-body">留言反馈</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu5.png" />
+          <div class="mui-media-body">视频专区</div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+        <a href="#">
+          <img src="/src/images/menu6.png" />
+          <div class="mui-media-body">联系我们</div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -40,20 +80,27 @@ export default {
   height: 200px;
 
   .mint-swipe-item {
-    &:nth-child(1) {
-      background-color: red;
-    }
-    &:nth-child(2) {
-      background-color: blue;
-    }
-    &:nth-child(3) {
-      background-color: cyan;
-    }
-
     img {
       width: 100%;
       height: 100%;
     }
   }
+}
+
+.mui-grid-view.mui-grid-9 {
+  background-color: #fff;
+  border: none;
+  img {
+    width: 60px;
+    height: 60px;
+  }
+
+  .mui-media-body {
+    font-size: 13px;
+  }
+}
+
+.mui-grid-view.mui-grid-9 .mui-table-view-cell {
+  border: 0;
 }
 </style>
