150cd3141e43543516a35d3c73802e7ae4a3cf28
unknown <>
2020-03-26 18:36:51 +0800

    设置 tabbar 路由激活高亮

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import app from './App.vue'
-
 // 引入 MUI 样式文件
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
-import router from './router'
-
 // 按需引入 mint-ui 组件
 import { Header } from 'mint-ui'
 Vue.component(Header.name, Header)
+
+import app from './App.vue'
+
+import router from './router'
 
 var vm = new Vue({
   el: '#app',
