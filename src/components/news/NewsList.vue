e1736272f1b64deff223146f33d4a41f855d1c64
unknown <>
2020-03-29 18:05:36 +0800

    新闻资讯列表数据请求并渲染

--- src/components/news/NewsList.vue
+++ src/components/news/NewsList.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
     <ul class="mui-table-view">
-      <li class="mui-table-view-cell mui-media">
+      <li class="mui-table-view-cell mui-media" v-for="item in newslist" :key="item.id">
         <a>
-          <img
-            class="mui-media-object mui-pull-left"
-            src="https://www.baidu.com/img/bd_logo1.png?where=super"
-          />
+          <img class="mui-media-object mui-pull-left" :src="item.img_url" />
           <div class="mui-media-body">
-            <h1>标题</h1>
+            <h1>{{ item.title }}</h1>
             <p class="mui-ellipsis">
-              <span>发表时间：2020：01、01</span>
-              <span>点击：0次</span>
+              <span>发表时间：{{ item.add_time }}</span>
+              <span>点击：{{ item.click }}次</span>
             </p>
           </div>
         </a>
@@ -21,7 +18,29 @@
 </template>
 
 <script>
-export default {};
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      newslist: []
+    };
+  },
+  created() {
+    this.getNewsList();
+  },
+  methods: {
+    getNewsList() {
+      this.$http.get("api/getnewslist").then(res => {
+        if (res.body.status === 0) {
+          this.newslist = res.body.message;
+        } else {
+          Toast("获取数据失败...");
+        }
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scope>
