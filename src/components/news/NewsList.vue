6057b3d0a196a2d986ece789d54b23dd3200b275
unknown <>
2020-03-29 17:37:45 +0800

    新闻资讯页面路由改造

---
+++ src/components/news/NewsList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>新闻资讯页面</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scope>
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import HomeContainer from './components/tabbar/HomeContainer.vue'
 import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
 import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
 
 var router = new VueRouter({
   routes: [
@@ -11,7 +12,8 @@ var router = new VueRouter({
     { path: '/home', component: HomeContainer },
     { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer }
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newslist', component: NewsList }
   ],
   linkActiveClass: 'mui-active'
 })
