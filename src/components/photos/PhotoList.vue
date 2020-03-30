7b98c4df3da3060c8f0b788345e35a4e743e9d69
unknown <>
2020-03-30 19:38:25 +0800

    根据 ID 获取图片数据并渲染

--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -11,10 +11,18 @@
             :class="['mui-control-item', item.id == 0 ? 'mui-active' : '']"
             v-for="item in cates"
             :key="item.id"
+            @tap="getPhotoListByCateId(item.id)"
           >{{ item.title }}</a>
         </div>
       </div>
     </div>
+
+    <!-- 图片列表区域 -->
+    <ul>
+      <li v-for="item in list" :key="item.id">
+        <img v-lazy="item.img_url" />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -25,11 +33,13 @@ import mui from "../../lib/mui/js/mui.js";
 export default {
   data() {
     return {
-      cates: []
+      cates: [],
+      list: []
     };
   },
   created() {
     this.getAllCatgory();
+    this.getPhotoListByCateId(0);
   },
   mounted() {
     mui(".mui-scroll-wrapper").scroll({
@@ -39,6 +49,7 @@ export default {
   },
   methods: {
     getAllCatgory() {
+      // 获取所有图片 分类数据
       this.$http.get("api/getimgcategory").then(result => {
         if (result.body.status === 0) {
           result.body.message.unshift({
@@ -46,6 +57,16 @@ export default {
             id: 0
           });
           this.cates = result.body.message;
+        } else {
+          Toast("获取数据失败...");
+        }
+      });
+    },
+    getPhotoListByCateId(cateId) {
+      // 根据分类 ID 获取图片数据
+      this.$http.get("api/getimages/" + cateId).then(result => {
+        if (result.body.status === 0) {
+          this.list = result.body.message;
         } else {
           Toast("获取数据失败...");
         }
@@ -58,5 +79,10 @@ export default {
 <style lang="scss" scoped>
 * {
   touch-action: pan-y;
+}
+img[lazy="loading"] {
+  width: 40px;
+  height: 300px;
+  margin: auto;
 }
 </style>
