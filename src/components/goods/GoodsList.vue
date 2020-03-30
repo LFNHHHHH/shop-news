eef96f102e9376a0f8e95b170185228139e812d5
unknown <>
2020-03-30 21:12:17 +0800

    加载商品数据并渲染

--- src/components/goods/GoodsList.vue
+++ src/components/goods/GoodsList.vue
@@ -1,63 +1,55 @@
 <template>
   <div class="goods-list">
-    <div class="goods-item">
-      <img
-        src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200119256512.jpg"
-        alt
-      />
-      <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+    <div class="goods-item" v-for="item in goodsList" :key="item.id">
+      <img :src="item.img_url" alt />
+      <h1 class="title">{{ item.title }}</h1>
       <div class="info">
         <p class="price">
-          <span class="now">￥899</span>
-          <span class="old">￥999</span>
+          <span class="now">￥{{ item.sell_price }}</span>
+          <span class="old">￥{{ item.market_price }}</span>
         </p>
         <p class="sell">
           <span>热卖中</span>
-          <span>剩60件</span>
+          <span>剩{{ item.stock_quaantity }}件</span>
         </p>
       </div>
     </div>
 
-    <div class="goods-item">
-      <img
-        src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200214471783.jpg"
-        alt
-      />
-      <h1 class="title">尼康(Nikon)D3300套机（18-55mm f/3.5-5.6G VRII）（黑色）</h1>
-      <div class="info">
-        <p class="price">
-          <span class="now">￥899</span>
-          <span class="old">￥999</span>
-        </p>
-        <p class="sell">
-          <span>热卖中</span>
-          <span>剩60件</span>
-        </p>
-      </div>
-    </div>
-
-    <div class="goods-item">
-      <img
-        src="http://ofv795nmp.bkt.clouddn.com//upload/201504/20/thumb_201504200119256512.jpg"
-        alt
-      />
-      <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
-      <div class="info">
-        <p class="price">
-          <span class="now">￥899</span>
-          <span class="old">￥999</span>
-        </p>
-        <p class="sell">
-          <span>热卖中</span>
-          <span>剩60件</span>
-        </p>
-      </div>
-    </div>
+    <mt-button type="danger" size="large" @click="getMore">加载更多</mt-button>
   </div>
 </template>
 
 <script>
-export default {};
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      pageIndex: 1,
+      goodsList: []
+    };
+  },
+  created() {
+    this.getGoodsList();
+  },
+  methods: {
+    getGoodsList() {
+      this.$http
+        .get("api/getgoods?pageindex=" + this.pageIndex)
+        .then(result => {
+          if (result.body.status === 0) {
+            this.goodsList = this.goodsList.concat(result.body.message);
+          } else {
+            Toast("获取数据失败...");
+          }
+        });
+    },
+    getMore() {
+      this.pageIndex++;
+      this.getGoodsList();
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
