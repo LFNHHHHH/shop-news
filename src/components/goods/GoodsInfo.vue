696d9021f30eea41b4008a1b4b54815fd3d2b143
unknown <>
2020-03-31 15:18:09 +0800

    获取商品详情页面轮播图数据

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,15 +1,86 @@
 <template>
-  <div>
-    <h3>商品详情页面</h3>
+  <div class="goodsinfo-container">
+    <!-- 商品轮播图区域 -->
+    <div class="mui-card">
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">这是一个最简单的卡片视图控件；卡片视图常用来显示完整独立的一段信息，比如一篇文章的预览图、作者信息、点赞数量等</div>
+      </div>
+    </div>
+
+    <!-- 商品购买区域 -->
+    <div class="mui-card">
+      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+      </div>
+    </div>
+
+    <!-- 商品参数区域 -->
+    <div class="mui-card">
+      <div class="mui-card-header">页眉</div>
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner">包含页眉页脚的卡片，页眉常用来显示面板标题，页脚用来显示额外信息或支持的操作（比如点赞、评论等）</div>
+      </div>
+      <div class="mui-card-footer">页脚</div>
+    </div>
   </div>
 </template>
 
+
 <script>
+import { Toast } from "mint-ui";
+
 export default {
   data() {
-    return {};
+    return {
+      id: this.$route.params.id,
+      lunbotu: []
+    };
+  },
+  created() {
+    this.getLunbotu();
+  },
+  methods: {
+    getLunbotu() {
+      this.$http.get("api/getthumimages/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          this.lunbotu = result.body.message;
+        } else {
+          Toast("获取数据失败...");
+        }
+      });
+    }
   }
 };
 </script>
 
-<style lang="stylus" scoped></style>
+<style lang="scss" scoped>
+.goodsinfo-container {
+  background-color: #eee;
+  overflow: hidden;
+
+  .now_price {
+    color: red;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  .mui-card-footer {
+    display: block;
+    button {
+      margin: 15px 0;
+    }
+  }
+
+  .ball {
+    width: 15px;
+    height: 15px;
+    border-radius: 50%;
+    background-color: red;
+    position: absolute;
+    z-index: 99;
+    top: 390px;
+    left: 146px;
+  }
+}
+</style>
