867a1cc6e042645d821bb8f73d0e7a45aecbb0da
unknown <>
2020-03-31 17:22:14 +0800

    图文介绍、商品评论页面完成

--- src/components/goods/GoodsDesc.vue
+++ src/components/goods/GoodsDesc.vue
@@ -1,12 +1,54 @@
 <template>
-  <div>
-    <h3>商品详情</h3>
+  <div class="goodsdesc-container">
+    <h3>{{ info.title }}</h3>
+
+    <hr />
+
+    <div class="content" v-html="info.content"></div>
   </div>
 </template>
 
 <script>
-export default {};
+import { Toast } from "mint-ui";
+
+export default {
+  data() {
+    return {
+      info: {}
+    };
+  },
+  created() {
+    this.getGoodsDesc();
+  },
+  methods: {
+    getGoodsDesc() {
+      this.$http
+        .get("api/goods/getdesc/" + this.$route.params.id)
+        .then(result => {
+          if (result.body.status === 0) {
+            this.info = result.body.message[0];
+          } else {
+            Toast("获取数据失败...");
+          }
+        });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.goodsdesc-container {
+  padding: 4px;
+  h3 {
+    font-size: 16px;
+    color: #226aff;
+    text-align: center;
+    margin: 15px 0;
+  }
+  .content {
+    img {
+      width: 100%;
+    }
+  }
+}
 </style>
