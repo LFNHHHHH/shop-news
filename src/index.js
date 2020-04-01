845d89a68972903d0ede7d7a2e5ff7b9c19cb761
unknown <>
2020-04-01 16:37:48 +0800

    将购物车数据存储在 localStorage 中

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ Vue.http.options.emulateJSON = true
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
+var car = JSON.parse(localStorage.getItem('car'))
+
 var store = new Vuex.Store({
   state: {
     // 将 购物车中的商品的数据，用一个数组存储起来，在 car 数组中，存储一些商品的对象
     // { id:商品的id, count: 要购买的数量, price: 商品的单价，selected: false  }
-    car: []
+    car: car
   },
   mutations: {
     addToCar(state, goodsinfo) {  // 添加到购物车
       var flag = false
+
       state.car.some(item => {  // 判断商品是否存在，存在则更新商品数量
         if (item.id === goodsinfo.id) {
           item.count += parseInt(goodsinfo.count)
@@ -28,9 +31,12 @@ var store = new Vuex.Store({
           return true
         }
       })
+
       if (flag === false) {  // 商品不存在则添加商品
         state.car.push(goodsinfo)
       }
+
+      localStorage.setItem('car', JSON.stringify(state.car))
     }
   },
   getters: {
