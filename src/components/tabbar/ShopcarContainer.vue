b059891461178f7fcf66b8ae39edefe3f0c8658a
unknown <>
2020-04-01 16:55:21 +0800

    绘制购物车页面

--- src/components/tabbar/ShopcarContainer.vue
+++ src/components/tabbar/ShopcarContainer.vue
@@ -1,12 +1,91 @@
 <template>
-  <div>
-    <h3>ShopcarContainer</h3>
+  <div class="shopcar-container">
+    <div class="goods-list">
+      <!-- 商品列表项区域 -->
+      <div class="mui-card">
+        <div class="mui-card-content">
+          <div class="mui-card-content-inner">
+            <mt-switch></mt-switch>
+            <img src="http://demo.dtcms.net/upload/201504/20/thumb_201504200059017695.jpg" />
+            <div class="info">
+              <h1>富家大室灵凤大父级的萨克雷父级的</h1>
+              <p>
+                <span class="price">￥2999</span>
+                <numbox></numbox>
+                <a href="#">删除</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 结算区域 -->
+    <div class="mui-card">
+      <div class="mui-card-content">
+        <div class="mui-card-content-inner jiesuan">
+          <div class="left">
+            <p>总计（不含运费）</p>
+            <p>
+              已勾选商品
+              <span class="red">5</span> 件， 总价
+              <span class="red">￥4999</span>
+            </p>
+          </div>
+          <mt-button type="danger">去结算</mt-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import numbox from "../subcomponents/shopcar_numbox.vue";
+
+export default {
+  components: {
+    numbox
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
+.shopcar-container {
+  background-color: #eee;
+  overflow: hidden;
+  .goods-list {
+    .mui-card-content-inner {
+      display: flex;
+      align-items: center;
+    }
+    img {
+      width: 60px;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+    h1 {
+      font-size: 13px;
+    }
+    .info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding-left: 10px;
+      .price {
+        color: red;
+        font-weight: bold;
+      }
+    }
+  }
+  .jiesuan {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .red {
+      color: red;
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+}
 </style>
