e37c64fdfdb8ef44ea49684c4daebd79be71777a
unknown <>
2020-04-01 12:31:00 +0800

    小球动画完善

--- src/components/goods/GoodsInfo.vue
+++ src/components/goods/GoodsInfo.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="goodsinfo-container">
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFlag" ref="ball"></div>
+    </transition>
+
     <!-- 商品轮播图区域 -->
     <div class="mui-card">
       <div class="mui-card-content">
@@ -23,7 +27,7 @@
           </p>
           <p>
             <mt-button type="primary" size="small">立即购买</mt-button>
-            <mt-button type="danger" size="small">加入购物车</mt-button>
+            <mt-button type="danger" size="small" @click="addToShopCar">加入购物车</mt-button>
           </p>
         </div>
       </div>
@@ -58,7 +62,8 @@ export default {
     return {
       id: this.$route.params.id,
       lunbotu: [],
-      goodsinfo: {}
+      goodsinfo: {},
+      ballFlag: true
     };
   },
   created() {
@@ -97,6 +102,29 @@ export default {
     goComment(id) {
       // 使用编程式导航跳转到商品评论页面
       this.$router.push({ name: "goodscomment", params: { id } });
+    },
+    addToShopCar() {
+      this.ballFlag = !this.ballFlag;
+    },
+    beforeEnter(el) {
+      el.style.transform = "translate(0, 0)";
+    },
+    enter(el, done) {
+      el.offsetWidth;
+      console.log(this.$refs.ball);
+      const ballPosition = this.$refs.ball.getBoundingClientRect();
+      const badgePosition = document
+        .getElementById("badge")
+        .getBoundingClientRect();
+      const xDist = badgePosition.left - ballPosition.left;
+      const yDist = badgePosition.top - ballPosition.top;
+
+      el.style.transform = `translate(${xDist}px, ${yDist}px)`;
+      el.style.transition = "all 1s ease";
+      done();
+    },
+    afterEnter(el) {
+      this.ballFlag = !this.ballFlag;
     }
   },
   components: {
@@ -131,8 +159,10 @@ export default {
     background-color: red;
     position: absolute;
     z-index: 99;
-    top: 390px;
-    left: 146px;
+    top: 54%;
+    left: 41%;
+    /* top: 362px;
+    left: 151px; */
   }
 }
 </style>
